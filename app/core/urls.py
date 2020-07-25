f18477e01a22a92d8b94a85212c0047615688130
stanislau <thedeadtwitch@gmail.com>
2020-07-25 12:15:07 +0300

    add login, register and profile users

--- app/core/urls.py
+++ app/core/urls.py
@@ -1,7 +1,11 @@
+from django.conf.urls import url
 from django.urls import path
-from app.core.views import MainPageView
+from app.core.views import MainPageView, LoginView, ProfilePage, RegisterView
 
 
 urlpatterns = [
+    url(r'^accounts/register/$', RegisterView.as_view(), name="register"),
+    path('accounts/login/', LoginView.as_view(), name="login"),
+    path('accounts/profile/', ProfilePage.as_view(), name="profile"),
     path('', MainPageView.as_view()),
 ]
--- app/core/views.py
+++ app/core/views.py
@@ -1,8 +1,48 @@
 from django.views.generic import TemplateView
+from django.contrib.auth import authenticate, login
+from django.shortcuts import redirect, render
+from django.urls import reverse
+from app.core.models import User
 
 
 class MainPageView(TemplateView):
     template_name = 'core/main_page.html'
 
 
+class LoginView(TemplateView):
+    template_name = "core/login.html"
+
+    def dispatch(self, request, *args, **kwargs):
+        context = {}
+        if request.method == 'POST':
+            username = request.POST['username']
+            password = request.POST['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect(reverse('profile'))
+            else:
+                context['error'] = "Логин или пароль неправильные"
+        return render(request, self.template_name, context)
+
+
+class ProfilePage(TemplateView):
+    template_name = "core/profile.html"
+
+
+class RegisterView(TemplateView):
+    template_name = "core/register.html"
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            email = request.POST.get('email')
+            password = request.POST.get('password')
+            password2 = request.POST.get('password2')
+
+            if password == password2:
+                User.objects.create_user(username, email, password)
+                return redirect(reverse("login"))
+
+        return render(request, self.template_name)
 
