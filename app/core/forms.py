f6fc27c7d99e0c3ac495f92355824931909245f5
stanislau <thedeadtwitch@gmail.com>
2020-07-28 13:17:48 +0300

    add Login and Signup pages

--- app/core/forms.py
+++ app/core/forms.py
@@ -3,15 +3,14 @@ from django.contrib.auth import get_user_model
 
 
 class SignUpForm(UserCreationForm):
+    first_name = forms.CharField(max_length=200)
+    last_name = forms.CharField(max_length=200)
 
     class Meta(UserCreationForm.Meta):
         model = get_user_model()
-        fields = ('username', 'email', 'password1', 'password2')
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
 
 
 class LoginForm(AuthenticationForm):
     def confirm_login_allowed(self, user):
-        if not get_user_model().is_active:
-            raise forms.ValidationError(
-                'Этот аккаунт не активен!'
-            )
+        pass
